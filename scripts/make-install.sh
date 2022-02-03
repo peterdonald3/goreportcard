116ff588de6212a4181724309687c4363bc906db
Peter Donald <peterdonald3@outlook.com>
2022-02-03 12:39:11 +0000

    updated make install

--- scripts/make-install.sh
+++ scripts/make-install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-go install ./vendor/github.com/alecthomas/gometalinter
-
 go install ./vendor/golang.org/x/lint/golint
 go install ./vendor/github.com/fzipp/gocyclo/cmd/gocyclo
 go install ./vendor/github.com/gordonklaus/ineffassign
 go install ./vendor/github.com/client9/misspell/cmd/misspell
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.44.0
